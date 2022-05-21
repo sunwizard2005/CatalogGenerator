0c543afb40c1522c6dda122b2d43eef8bd19e2ab
Jason Mills <sunray@twyst.org>
2022-05-21 06:40:36 -0400

    Use RestSharp to fetch Catalog data

--- CatalogGenerator/Program.cs
+++ CatalogGenerator/Program.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RestSharp;
 using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Security.Authentication;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -16,12 +18,38 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddTransient(s =>
 {
-    string catalogSource = File.ReadAllText("test-catalog.json");
+    string catalogSource;// = File.ReadAllText("test-catalog.json");
     //catalogSource = Task.Run(() =>
     //{
-    //    HttpClient client = new HttpClient();
-    //    return client.GetStringAsync("https://trello.com/b/S9VlWoZn.json");
+    //    async Task<string> GetURL()
+    //    {
+    //
+    //
+    //        using HttpClient client = new HttpClient(new HttpClientHandler
+    //        {
+    //            PreAuthenticate = true,
+    //            UseDefaultCredentials = true, AllowAutoRedirect = true,
+    //            UseCookies = true,
+    //            SslProtocols = SslProtocols.Tls12
+    //        });
+    //        try
+    //        {
+    //            var a = await client.GetStringAsync("https://trello.com/b/S9VlWoZn.json");
+    //            return a;
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw;
+    //        }
+    //
+    //    }
+    //
+    //    return GetURL();
     //}).Result;
+    RestClient client = new RestClient("https://trello.com");
+    RestRequest request = new RestRequest("/b/S9VlWoZn.json", Method.GET);
+    var response = client.Execute(request);
+    catalogSource = response.Content;
 
     Catalog catalog = JsonSerializer.Deserialize<Catalog>(catalogSource) ?? throw new Exception("Unable to load catalog");
     return catalog;
